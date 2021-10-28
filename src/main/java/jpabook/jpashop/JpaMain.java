044c488243a26d610289278337ed64399bb098d5
kkkimhyeon <zofldnj1@gmail.com>
2021-10-28 15:38:30 +0900

    고급매핑 실전 예제 커밋

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.domain.Member;
 
 import javax.persistence.EntityManager;
@@ -17,6 +19,40 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
+
+            Member member = new Member();
+            member.setName("herry");
+            em.persist(member);
+
+            Category category = new Category();
+            category.setName("메뉴A");
+            em.persist(category);
+
+            Category category2 = new Category();
+            category2.setName("메뉴A-1");
+            category2.setParent(category);
+            em.persist(category2);
+
+            Category category3 = new Category();
+            category3.setName("메뉴A-2");
+            category3.setParent(category);
+            em.persist(category3);
+
+            Category category4 = new Category();
+            category4.setName("메뉴B");
+
+//            em.flush();
+            Category findCategory = em.find(Category.class, category.getId());
+            System.out.println("@@@ " + findCategory.getChild());
+            for (Category c : findCategory.getChild()) {
+                System.out.println(c.getName());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -24,7 +60,6 @@ public class JpaMain {
             em.close();
             emf.close();
         }
-
 
     }
 }
--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Category {
+public class Category extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -27,4 +27,43 @@ public class Category {
     )
     private List<Item> items = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public List<Category> getChild() {
+        return child;
+    }
+
+    public void setChild(List<Category> child) {
+        this.child = child;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
